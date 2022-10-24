c3be94cf47197b1ec9774db812509bf04a69e90c
Vitaly Statsura <vstatsura@stagewood.com>
2022-10-24 21:30:13 +0300

    fix ignore

--- src/services/workers/service-worker.ts
+++ src/services/workers/service-worker.ts
@@ -1,5 +1,6 @@
+/* eslint-disable prefer-regex-literals */
+/* eslint-disable no-underscore-dangle */
 /// <reference lib="webworker" />
-/* eslint-disable no-restricted-globals */
 
 // This service worker can be customized!
 // See https://developers.google.com/web/tools/workbox/modules
