5968fe446d7c7e4c1f22e0e59312985d27c3a394
Vitaly Statsura <vstatsura@stagewood.com>
2022-10-24 21:16:47 +0300

    fix eslint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,54 @@
 module.exports = {
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  parser: '@typescript-eslint/parser',
   extends: [
     '@pswind/eslint-config',
     'plugin:jest/recommended',
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
   ],
-  plugins: ['@typescript-eslint', 'jest', 'prettier'],
+  overrides: [
+    {
+      files: ['*.js', '*.jsx'],
+      rules: {
+        '@typescript-eslint/*': ['off'],
+      },
+    },
+    {
+      files: [
+        '*.spec.ts',
+        '*.spec.tsx',
+        '*.spec.js',
+        '*.spec.jsx',
+        '*.test.ts',
+        '*.test.tsx',
+        '*.test.js',
+        '*.test.jsx',
+      ],
+      rules: {
+        '@typescript-eslint/*': 'off',
+      },
+    },
+  ],
+  plugins: ['@typescript-eslint', 'jest', 'prettier', 'react'],
   parserOptions: {
-    project: './tsconfig.json', // Specify path to your tsconfig file
+    ecmaVersion: 13,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+    project: ['./tsconfig.json'],
   },
   rules: {
     'react/jsx-uses-react': 'off', // react 17+ doesn't need react imports
     'react/react-in-jsx-scope': 'off', // react 17+ doesn't need react imports
     'eol-last': ['error', 'always'],
-  }
-}
+    'import/no-unresolved': 'off', // todo
+    'import/named': 'off',
+    'import/extensions': 'off',
+  },
+};
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import * as serviceWorkerRegistration from './services/workers/serviceWorkerRegistration';
 import reportWebVitals from './services/workers/reportWebVitals';
+import { Test } from './modules';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
   <React.StrictMode>
-  </React.StrictMode>
+    <Test />
+  </React.StrictMode>,
 );
 
 // If you want your app to work offline and load faster, you can change
