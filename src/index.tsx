239be04d4907fea90f330daaafb756af8871711c
Vitaly Statsura <vstatsura@stagewood.com>
2022-10-24 20:38:05 +0300

    basic folder structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './services/workers/serviceWorkerRegistration';
+import reportWebVitals from './services/workers/reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
   </React.StrictMode>
 );
 
